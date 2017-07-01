f1b0b92dc4666c78420ec5d03dac22adacd80229
Anjali_Newbie <anjalie@gmail.com>
2017-07-01 23:29:52 +0530

    Logistic Regression

--- ML Model/ML_Assignment - R.R
+++ ML Model/ML_Assignment - R.R
@@ -6,6 +6,9 @@ library(randomForest)
 library(caret)
 library(ggplot2)
 library(reshape2)
+library(pastecs)
+library(vegan)
+library(DMwR)
 
 attr = c('id', 'age', 'exp', 'inc', 'zip', 'family', 
          'ccavg', 'edu', 'mortgage', 'loan', 
@@ -21,16 +24,78 @@ attr = setdiff(attr, drop_Attr)
 data = data[, attr]
 str(data)
 
-data$loan = as.factor(data$loan)
+options(scipen=100)
+options(digits=2)
+stat.desc(data)
+
+#converting to approproate data types - 
+cat_Attr = c("family","edu","securities","cd","online","cc","loan")
+num_Attr = setdiff(attr, cat_Attr)
+cat_Data <- data.frame(sapply(data[,cat_Attr], as.factor))
+num_Data <- data.frame(sapply(data[,num_Attr], as.numeric))
+reg_Data <- decostand(num_Data, "range") #normalising numeric data
+data <- cbind(reg_Data, cat_Data)
 str(data)
+rm(attr, cat_Attr, num_Attr, drop_Attr,cat_Data, num_Data, reg_Data)
+
+# Split dataset into train and test
+set.seed(123)
+
+train_RowIDs = sample(1:nrow(data), nrow(data)*0.7)
+train_Data = data[train_RowIDs,]
+test_Data = data[-train_RowIDs,]
+rm(train_RowIDs)
+
+#check the split with respect to target attribute - 
+
+table(train_Data$loan)
+table(test_Data$loan)
 
 
-#Visualisation of data and distribution - 
-par(mfrow = c(3,3))
-ggplot(data, aes(x = age, y = factor(loan))) + geom_boxplot()
-ggplot(data, aes(x = factor(loan), y = exp)) + geom_boxplot()
-ggplot(data, aes(x = factor(loan), y = inc)) + geom_boxplot()
-ggplot(data, aes(x = factor(loan), y = ccavg)) + geom_boxplot()
-ggplot(data, aes(x = factor(loan), y = mortgage)) + geom_boxplot()
-ggplot(data, aes(x = factor(loan), y = factor(edu))) + geom_boxplot()
+# Logistic Regression Model
+Unibank_glm = glm(loan~., data = train_Data, family=binomial)
+summary(train_glmodel)
+car::vif(train_glmodel)
+
+#Accuracy on the training set
+predictTrain = predict(Unibank_glm, type="response", newdata = train_Data)
+
+# Confusion matrix with threshold of 0.5
+conf_matrix_train = table(train_Data$loan, predictTrain > 0.5)
+
+# Accuracy on Train Set
+accu_Train= sum(diag(conf_matrix_train))/sum(conf_matrix_train)
+accu_Train
+
+
+# Predictions on the test set
+predictTest = predict(Unibank_glm, type="response", newdata=test_Data)
+
+# Confusion matrix with threshold of 0.5
+conf_matrix_test = table(test_Data$loan, predictTest > 0.5)
+
+# Accuracy on Test Set
+
+accu_Test = sum(diag(conf_matrix_test))/sum(conf_matrix_train)
+accu_Test
+
+# Confusion matrix with threshold of 0.9
+table(test_Data$loan, predictTest > 0.9)
+# Confusion matrix with threshold of 0.7
+table(test_Data$loan, predictTest > 0.7)
+# Confusion matrix with threshold of 0.5
+table(test_Data$loan, predictTest > 0.5)
+# Confusion matrix with threshold of 0.3
+table(test_Data$loan, predictTest > 0.3)
+# Confusion matrix with threshold of 0.1
+table(test_Data$loan, predictTest > 0.1)
+
+# Test set AUC 
+library(ROCR)
+ROCRpred = prediction(predictTest, test_Data$loan)
+as.numeric(performance(ROCRpred, "auc")@y.values)
+ROCRperf <- performance(ROCRpred, "tpr", "fpr")
+par(mfrow=c(1,1))
+plot(ROCRperf, colorize = TRUE, print.cutoffs.at=seq(0,1,by=0.1), text.adj=c(-0.2,1.7))
+
 
